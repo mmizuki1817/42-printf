a920386ec964bf80a7f3d035d4d59aff1775257a
mmizuki <mmizuki1817@gmail.com>
2022-08-05 16:11:40 +0900

    update

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: mimatsub <mimatsub@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/17 19:44:22 by mimatsub          #+#    #+#             */
-/*   Updated: 2022/08/05 16:05:55 by mimatsub         ###   ########.fr       */
+/*   Updated: 2022/08/05 16:11:24 by mimatsub         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,10 @@
 
 # include <stdarg.h>
 # include <unistd.h>
+
+size_t	ft_putstr_fd(char *s);
+
+int	ft_putchar_fd(int c);
 
 void	ft_put_base(unsigned long long num, char c, int base);
 
