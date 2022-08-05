7a9a5c20240788bd45ecae1809b7fa53193612f6
mizuki <84909407+mmizuki1817@users.noreply.github.com>
2022-08-06 01:07:16 +0900

    Update ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -18,15 +18,15 @@
 
 size_t	ft_putstr_fd(char *s);
 
-int		ft_putchar_fd(int c);
+size_t		ft_putchar_fd(int c);
 
-void	ft_put_base(unsigned long long num, char c, int base);
+size_t	ft_put_base(unsigned long long num, char c, int base);
 
-int		ft_treat_int(int i);
+size_t	ft_treat_int(int i);
 
-int		ft_treat_base(unsigned int num, char c);
+size_t	ft_treat_base(unsigned int num, char c);
 
-int		ft_treat_point(unsigned long long p);
+size_t	ft_treat_point(unsigned long long p);
 
 int		ft_printf(const char *input, ...);
 
